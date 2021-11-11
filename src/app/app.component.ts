3dc528f2d3fc85a712ffbeb7f30b4a0fb5d0006b
Yousif <ybeeai@gmail.com>
2021-11-11 16:17:37 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChirpusPizza';
+  menuItems: MenuItem[] = [
+    {item: "Breadsticks", category:"Sides", price:4.99},
+    {item: "Wings", category:"Sides", price: 8.99},
+    {item: "Chicken Fingers", category:"Dinner", price: 11.99},
+    {item: "Pizza", category: "Dinner", price: 11.99},
+    {item: "Caesar Salad", category:"Salads", price: 5.99},
+    {item:"Cinnamon Roll", category:"Dessert", price: 8.99}
+  ];
 }
---
+++ src/app/menu-item.ts
@@ -0,0 +1,5 @@
+export interface MenuItem {
+item:string,
+category:string,
+price:number
+}
